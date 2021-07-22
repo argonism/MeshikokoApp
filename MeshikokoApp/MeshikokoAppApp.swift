e364284e12941c3f1378951a5ef75cfbd9e741b7
k-usuha <kouta528bado@yahoo.co.jp>
2021-07-23 03:24:47 +0900

    Initial Commit

---
+++ MeshikokoApp/MeshikokoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshikokoAppApp.swift
+//  MeshikokoApp
+//
+//  Created by 薄羽皐太 on 2021/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MeshikokoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
